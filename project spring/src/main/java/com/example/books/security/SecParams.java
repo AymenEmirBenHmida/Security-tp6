26619868c697c84ac7fc4a78dcfa7281885f5928
AymenEmirBenHmida <80763461+AymenEmirBenHmida@users.noreply.github.com>
2021-11-21 17:24:07 +0100

    Add files via upload

---
+++ project spring/src/main/java/com/example/books/security/SecParams.java
@@ -0,0 +1,9 @@
+package com.example.books.security;
+
+public interface SecParams {
+	public static final long EXP_TIME = 10*24*60*60;
+	public static final String SECRET = "baymenemir@gmail.com";
+	public static final String PREFIX = "Bearer ";
+	
+
+}
